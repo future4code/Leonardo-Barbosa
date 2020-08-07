6a7744b30885ada67d5ba8d3d882a35bed8c2b8f
unknown <leo.oliveira.b@gmail.com>
2020-08-07 14:15:32 -0300

    Exercício-2-Finalizado

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,2 +1,27 @@
 // Exercícios de interpretação de código
 
+// 1 - Será impresso R$ 543 / com o dólar a R$ 5,43
+// 2 - Será impresso no novoMontante 165 e no segundoMontante "Tipo de investimento informado incorreto!"
+// 3 - Será impresso no primeiro "Quantidade total de números 14", no segundo array 6 e no terceiro 8
+// 4 - 
+
+// Exercícios de Lógica de Programação
+
+// 1 - //while, For e For... of... 
+//     const numeros = [45, 65, 2, 77, 12]
+//     for (let numero of numeros){
+//         console.log(numero)
+//     }
+
+// 2 - A False 
+//   - B False  
+//   - C False 
+//   - D True 
+//   - E True
+
+// 3 - 
+const quantidadeDeNumerosPares
+let i = 0 
+while(i <= quantidadeDeNumerosPares){
+    console.log(i*2)
+}
