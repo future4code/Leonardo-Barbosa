8475f3c837d880c54edcc1e72d5ca1f5e0a402dc
unknown <leo.oliveira.b@gmail.com>
2020-09-17 17:35:41 -0300

    LoginPage pronta

--- semana10/labex/src/Components/LoginPage.jsx
+++ semana10/labex/src/Components/LoginPage.jsx
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
+import styled from 'styled-components'
+import labex from '../img/labex.png'
+import Button from '@material-ui/core/Button'
+import {goToHomePage} from '../Router/GoToPages'
+import {goToListTripsPage} from '../Router/GoToPages'
+
+
+const ContainerHome = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  gap:20px;
+  `
+
+const ContainerMain = styled.div`
+  display:flex;
+  align-items:center;
+  margin:20px;
+  Button{
+     margin-left:50px;
+  }
+  
+  `
+
 
 
 function LoginPage() {
@@ -41,9 +65,17 @@ function LoginPage() {
   
   return (
     <div>
-      <TextField label="Email" variant="outlined" required value={emailValue} onChange={handleEmailChange} />
-      <TextField label="Senha" variant="outlined" required value={passwordValue} onChange={handlePasswordChange} />
-      <button onClick={handleLogin}>Fazer Login</button>
+      <ContainerMain>
+      <img src={labex}></img>
+        <Button variant="contained" color="primary" onClick={() => goToHomePage(history)}>Home</Button>
+        <Button variant="contained" color="primary" onClick={() => goToListTripsPage(history)}>Viagens</Button>
+      </ContainerMain>
+      <ContainerHome>
+        <h2>Login para Administrador</h2>
+        <TextField label="Email" variant="outlined" required value={emailValue} onChange={handleEmailChange} />
+        <TextField label="Senha" variant="outlined" required value={passwordValue} onChange={handlePasswordChange} />
+        <button onClick={handleLogin}>Fazer Login</button>
+      </ContainerHome>
     </div>
   )
 }
