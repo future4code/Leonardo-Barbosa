c0e7f8dbcdacdd0f058b1abbfc5726066f01a8b6
Leobroud <leo.oliveira.b@gmail.com>
2020-10-19 16:58:09 -0300

    exercicio da aula 40

---
+++ semana14/aula40/index.js
@@ -0,0 +1,36 @@
+process.argv[0]; //SEMPRE É O NODE
+process.argv[1]; //SEMPRE O ARQUIVO QUE ESTAMOS RODANDO
+
+
+// EXERCÍCIO 1 
+
+// A - Acessamos através do process.argv 
+
+// B 
+
+// const nome = process.argv[2];
+// const idade = Number(process.argv[3]);
+// const soma = idade + Number(process.argv[4]);
+
+// console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+// C 
+
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${soma}.`)
+
+
+// EXERCÍCIO 2 -
+
+    // const operation = process.argv[2];
+    // const numberOne = Number(process.argv[3]);
+    // const numberTwo = Number(process.argv[4]);
+
+    //     if ( operation === 'add') {
+    //         numberOne + numberTwo
+    //     } else if ( operation === 'sub') {
+    //         numberOne - numberTwo
+    //     } else if ( operation === 'mult') {
+    //         numberOne * numberTwo
+    //     } else if ( operation === 'div') {
+    //         numberOne / numberTwo
+    //     }
