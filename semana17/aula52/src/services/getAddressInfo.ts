611120aa0d7f99000b83eb82101ed60b9592480f
Leobroud <leo.oliveira.b@gmail.com>
2020-11-11 14:55:17 -0300

    exercício da aula 52

---
+++ semana17/aula52/src/services/getAddressInfo.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { v4 } from "uuid"
+
+const URL_ADDRESS = "https://viacep.com.br/ws/"
+
+type address = {
+  logradouro: string,
+  bairro: string,
+  cidade: string,
+  estado: string
+}
+
+export async function getAddressInformation(cep: number): Promise<address> {
+
+  const result = await axios.get(`${URL_ADDRESS}/${cep}/json`)
+
+  return { 
+    logradouro: result.data.logradouro,
+    bairro: result.data.bairro,
+    cidade: result.data.cidade,
+    estado: result.data.uf
+  }
+
+
+}
