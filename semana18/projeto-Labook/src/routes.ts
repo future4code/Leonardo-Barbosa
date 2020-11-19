20862771be9b50da498702982143a2d076a697e0
Leobroud <leo.oliveira.b@gmail.com>
2020-11-19 16:26:35 -0300

    criando endpoint de post

--- semana18/projeto-Labook/src/routes.ts
+++ semana18/projeto-Labook/src/routes.ts
@@ -4,5 +4,8 @@ import UserController from './controllers/userController'
 const routes = Router()
 
 routes.post('/users/signup', UserController.create)
+routes.post('/users/login', UserController.login)
+routes.post('/posts/create', )
+routes.post('/posts/:id', )
 
 export default routes
