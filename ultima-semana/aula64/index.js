ab874a98376bbf58c5cedfa4a50622c6a634589a
Leobroud <leo.oliveira.b@gmail.com>
2021-01-04 16:46:30 -0300

    exercício 1

---
+++ ultima-semana/aula64/index.js
@@ -0,0 +1,18 @@
+// Exercício 1
+const words = (inputA, inputB) => {
+    if (Math.abs(inputB.length - inputA.length) > 1)
+        return false;
+    if (inputA.length > inputB.length)
+        return inputA.includes(inputB);
+    if (inputB.length > inputA.length)
+        return inputB.includes(inputA);
+    let charCount = 0;
+    for (let i = 0; i < inputA.length; i++) {
+        if (inputA[i] !== inputB[i])
+            charCount++;
+    }
+    return charCount === 1;
+};
+console.log(words("banana", "banan"));
+console.log(words("panana", "banana"));
+console.log(words("amora", "amo"));
